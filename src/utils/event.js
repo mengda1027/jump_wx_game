a4f42f062110e35381bb4a9ae5852ff61cf962b8
mengda1027 <mengda1027@qq.com>
2020-10-19 17:14:42 +0800

    add event util class

---
+++ src/utils/event.js
@@ -0,0 +1,26 @@
+class Event {
+  constructor(sender) {
+    this._sender = sender
+    this._listener = []
+  }
+
+  /**
+   * 绑定
+   * @param {function} callback
+   */
+  attach(callback) {
+    this._listener.push(callback)
+  }
+
+  /**
+   * 触发
+   * @param {any} args
+   */
+  notify(args) {
+    for (let i = 0; i < this._listener.length; i++) {
+      this._listener[i](sender, args)
+    }
+  }
+}
+
+export default Event
